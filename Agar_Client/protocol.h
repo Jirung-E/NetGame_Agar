0f8bb9029d8bc4de15dee35cb2e5ce0a6d96204d
znffna[Laptop] <znffna@gmail.com>
2024-11-15 12:03:29 +0900

    struct CS_ACTION_PACKET 작성

---
+++ Agar_Client/protocol.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define CS_ACTION 1
+
+
+#pragma pack(push, 1)
+struct CS_ACTION_PACKET {
+	char type;  // CS_ACTION
+	char flags; // 0x01: split, 0x02: spit
+	float mx;   // [-1.0f ~ 1.0f]
+	float my;   // [-1.0f ~ 1.0f]
+};
+#pragma pack(pop)
